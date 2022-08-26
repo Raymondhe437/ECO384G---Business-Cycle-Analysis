e45688fdf3234f33b757a7e2350f90bde87039e5
Raymond He <34176635+Raymondhe437@users.noreply.github.com>
2022-08-26 10:36:54 -0500

    Add files via upload

---
+++ Problem Set 1/eco384g_pset1.jl
@@ -0,0 +1,26 @@
+#Code for ECO384G Problem Set 1  - Impulse Responses and Phillips Curves
+#Author: Raymond He
+#Last updated - August 25th 2022
+
+##### Preamble #####
+cd("/Users/raymondhe/Documents/ECO384G-Trade 1/Assessments/PSet 1")
+
+using OhMyREPL
+using Plots
+using LinearAlgebra
+using LocalProjections
+using XLSX
+using FileIO
+using DataFrames
+using ExcelFiles
+
+
+##### Loading data #####
+#Romer and Romer 2004 data
+
+test = CSV.read("Data/RomerandRomerDataAppendix.xls",
+                DataFrame;
+                normalizenames = true)
+
+
+is this going to work 
